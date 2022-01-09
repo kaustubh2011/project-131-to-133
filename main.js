35d78ba9aed953d7a673d1746e3137257e58aa41
kaustubh2011 <81859002+kaustubh2011@users.noreply.github.com>
2022-01-09 16:33:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function human()
+{
+    window.location="human.html";
+}
+function vehicle()
+{
+    window.location="vehicle.html";
+}
+function bed()
+{
+    window.location="bed.html";
+}
+function cake()
+{
+    window.location="cake.html";
+}
